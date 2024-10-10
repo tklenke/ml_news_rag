7bc119342738db96097da454ea47f0f2d647b73c
tklenke <tklenke@gmail.com>
2024-10-10 17:33:55 -0400

    creating of embedder to embed large corpus of documents in a restartable manner

---
+++ embedder/Readme.md
@@ -0,0 +1,35 @@
+# Embed Technical Corpus
+
+Script will embed a corpus of test data via one embedding model and store 
+the vector data into a ChromaDB. Script will build a list of documents to embed and start
+embedding each file.  the order of files will be random, but a list will be kept such
+that the script can be stopped and restarted saving progress.
+
+## Workflow
+
+### Set up Docker Containers
+
+Build two docker containers required.  One for the ChromaDB and one for Ollama and the embedding models.
+ChromaDB will store data persistantly and Ollama will download relevant models for embedding and 
+answer testing.  Run the docker-compose from the parent directory
+
+```
+docker-compose build
+docker-compose up -d
+docker-compose ps
+docker exec modeltesting-ollama-1 ollama list
+```
+*might need to confirm the name of the ollama container for the list*
+
+### Ensure python has required libraries
+```
+pip install -r requirements. txt
+```
+
+### Run the Embedder
+```
+python embedData.py
+```
+
+
+
---
+++ embedder/chroma_functions.py
@@ -0,0 +1,51 @@
+import chromadb
+from embed_functions import readtextfile, chunksplitter, getembedding
+
+def getclient(host="localhost", port="8000"):
+  print(f"  cf:opening chroma on {host}:{port}...")
+  chromaclient = chromadb.HttpClient(host, port)
+  return chromaclient
+
+def getcollection(chromaclient, dbname="defaultdb", initialize=False):
+  print(f"  cf:opening {dbname}...")
+  if initialize:
+      if any(dbname == collection.name for collection in chromaclient.list_collections()):
+        print(f"  cf:found duplicate collection {dbname}. Removing...")
+        chromaclient.delete_collection(dbname)
+  collection = chromaclient.get_or_create_collection(dbname, metadata={"hnsw:space": "cosine"}  )
+  return(collection)
+
+#given a full path, return the filename without extension
+def get_id(path):
+    filename = path.split("/")[-1]
+
+    # Split the filename based on the last dot (.)
+    parts = filename.rsplit(".", 1)
+
+    # Check if there's only one part (no extension)
+    if len(parts) == 1:
+        return filename
+    # Otherwise, return the part before the dot
+    else:
+        return parts[0]
+
+
+def embed_file(textdocspath, model="nomic-embed-text", chromacollection=None, chunk_size=100 ): 
+  if chromacollection is None:
+    chromaclient = getclient()
+    chromacollection = getcollection(chromaclient)
+
+  text_data = readtextfile(textdocspath)
+
+  for filename, text in text_data.items():
+    chunks = chunksplitter(text, chunk_size)
+    embeds = getembedding(chunks, model)
+    chunknumber = list(range(len(chunks)))
+    ids = [get_id(filename) + str(index) for index in chunknumber]
+    metadatas = [{"source": filename} for index in chunknumber]
+    chromacollection.add(ids=ids, documents=chunks, embeddings=embeds, metadatas=metadatas)
+
+  if __name__ == "__main__":
+    print(f"testing chroma funtions...")
+    embed_file(textdocspath="../data/test")
+    print(f"done")
